18b9bb74ea022dc532fd96fcb171de19d1509b1a
Platformer10101 <platformer10101@gmail.com>
2024-08-14 14:43:57 -0400

    Add files via upload

---
+++ Number_Swapping.py
@@ -0,0 +1,14 @@
+x = input("Enter Value of x:")
+y = input("Enter Value of y:")
+z = input("Enter Value of z:")
+
+temp = x
+temp2 = y
+temp3 = z
+x = temp2
+y = temp3
+z = temp
+
+print("Value of x after swapping:", x)
+print("Value of y after swapping", y)
+print("Value of z after swapping", z)
