63ad757de124df5d9dfc0c7880c56eaf96f20710
Platformer10101 <platformer10101@gmail.com>
2024-08-15 19:00:09 -0400

    Add files via upload

---
+++ Exam_Elegiblilty.py
@@ -0,0 +1,9 @@
+age = int(input("Enter age:"))
+grade = float(input("Enter your current grade in %:"))
+if age==15:
+    if grade>=50:
+        print("You are elegible for the Grade 10 coding exam")
+    else:
+        print("You are not elegible for the exam")
+else:
+    print("you are not elegible for the exam")
